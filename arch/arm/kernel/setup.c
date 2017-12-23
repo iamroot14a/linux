1e0dc0fc00084450aa518cf554e0580c1cb5c0bc
ssamdaq <ssamdaq@gmail.com>
2017-12-23 21:28:17 +0900

    20171223 last day of 2017 until smp_setup

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -581,21 +581,47 @@ void notrace cpu_init(void)
 
 u32 __cpu_logical_map[NR_CPUS] = { [0 ... NR_CPUS-1] = MPIDR_INVALID };
 
+/* K14AB: 2017년 12월 23일 20:51:40
+ * ------------------------------
+ * MPIDR_INVALID 
+ * ~ 0x FF FF FF = 0x 00 00 00
+ */
+
+
+
 void __init smp_setup_processor_id(void)
 {
 	int i;
 	u32 mpidr = is_smp() ? read_cpuid_mpidr() & MPIDR_HWID_BITMASK : 0;
 	u32 cpu = MPIDR_AFFINITY_LEVEL(mpidr, 0);
 
+	/* K14AB: 2017년 12월 23일 20:26:02
+	 * ------------------------------
+	 * MPIDR_AFFINITY_LEVEL(mpidr, 0) --> ((mpidr >> ( 8 * level)) & 0b 1111 1111)
+	 */
 	cpu_logical_map(0) = cpu;
 	for (i = 1; i < nr_cpu_ids; ++i)
 		cpu_logical_map(i) = i == cpu ? 0 : i;
 
+	/* K14AB: 2017년 12월 23일 21:10:52
+	 * ------------------------------
+	 *  cpu_logical_map(i) = i == cpu ? 0 : i;
+	 *   괄호를 넣어 보면..
+	 * (cpu_logical_map(i) = ((i == cpu) ? 0 : i);
+	 */
+		
 	/*
 	 * clear __my_cpu_offset on boot CPU to avoid hang caused by
 	 * using percpu variable early, for example, lockdep will
 	 * access percpu variable inside lock_release
 	 */
+
+	/* K14AB: 2017년 12월 23일 21:22:21
+	 * ------------------------------
+	 * 올해 마지막 시간 ~ 
+	 * 다들 수고 하셨습니다\( ^0^ )/
+	 */
+
 	set_my_cpu_offset(0);
 
 	pr_info("Booting Linux on physical CPU 0x%x\n", mpidr);
